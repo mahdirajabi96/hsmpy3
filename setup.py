5c4691be6c59446514cba91b2a3d0fc1e48ee373
mahdirajabi96 <mahdi.rajabi.96@gmail.com>
2019-07-10 13:47:15 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,24 +1,22 @@
 from distutils.core import setup
 setup(
-  name = 'hsmpy3',         # How you named your package folder (MyLib)
-  packages = ['hsmpy3'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
-  license='bsd-2-clause',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository  
-  description = 'Highway Safety Manual Calculations',   # Give a short description about your library
-  author = 'Mahdi Rajabi',                   # Type in your name
-  author_email = 'mahdi.rajabi.96@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/mahdirajabi96',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['Highway Safety Manual'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'arcpy',
-      ],
+  name = 'hsmpy3',
+  packages = ['hsmpy3'],
+  version = '1.0',
+  license='bsd-2-clause',
+  description = 'Highway Safety Manual Calculations',
+  author = 'Mahdi Rajabi',
+  author_email = 'mahdi.rajabi.96@gmail.com',
+  url = 'https://github.com/mahdirajabi96',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['Highway Safety Manual'],
+  install_requires=['arcpy'],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers', 
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: GNU General Public License v3.0',  
+    'Programming Language :: Python :: 3',     
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
